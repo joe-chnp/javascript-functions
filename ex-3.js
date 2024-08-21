e078921b14a3119fecd46a40ad38f0e62fae4435
Joe <chanaphat.sip@gmail.com>
2024-08-21 17:50:47 +0700

    bp-5-joe

--- ex-3.js
+++ ex-3.js
@@ -1,1 +1,5 @@
 // Start coding here
+const add = (a, b) => a + b;
+const substract = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+const divide = (a, b) => a / b;
--- ex-4.js
+++ ex-4.js
@@ -1,2 +1,18 @@
 // Start coding here
-let calculator = {};
+const add = (a, b) => a + b;
+const substract = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+const divide = (a, b) => a / b;
+
+let calculator = {
+    add: add,
+    substract: substract,
+    multiply: multiply,
+    divide: divide
+};
+
+const addResult = calculator.add(10, 20);
+console.log(addResult);
+
+const divideResult = calculator.divide(3000, 10);
+console.log(divideResult);
